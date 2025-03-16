7a668fd17949e5fae929fb0d4238c4e2d49eb2bc
Igor Buyanov O <buyanov.igor.o@yandex.ru>
2025-03-16 18:39:04 +0300

    added explicit type confersion for dict

--- model_repository/post_processing/1/model.py
+++ model_repository/post_processing/1/model.py
@@ -33,7 +33,8 @@ class TritonPythonModel:
         
         with open("/models/post_processing/1/id2label.json", encoding='utf-8') as f:
             id2label = json.load(f)
-        self.id2label = {v:k for k,v in id2label.items()}
+        print(id2label)
+        self.id2label = {int(v):k for k,v in id2label.items()}
 
         # Get OUTPUT0 configuration
         #output0_config = pb_utils.get_output_config_by_name(
