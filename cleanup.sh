fcba88a54db18436d0c5fc8b26225c7653e7557d
Igor Buyanov O <buyanov.igor.o@yandex.ru>
2025-03-16 18:17:40 +0300

    added class counter util; added optionality for some args

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,3 @@
-git checkout .
 rm -rf model_repository/model_onnx/*.pbtxt
 rm -rf model_repository/post_processing/*.pbtxt
 rm -rf model_repository/ensemble_model/*.pbtxt
@@ -6,3 +5,8 @@ rm -rf model_repository/model_onnx/1/model.onnx
 rm -rf model_repository/post_processing/1/id2label.json
 rm -rf model_repository/text_preprocessing/1/tokenizer/*
 rm -rf temp_model
+if [ ! -z "$1" ];
+then
+    rm -rf model_repository/"$1"
+fi
+git checkout ./model_repository
--- make_triton_image.sh
+++ make_triton_image.sh
@@ -1,12 +1,34 @@
+set -e
+
 MODEL_PATH=$1
 TOKENIZER_PATH=$2
 ID2LABEL_PATH=$3
-NUM_CLASSES=$4
-MODEL_NAME=$5
-MAX_BATCH_SIZE=$6
+
+if [ ! -z "$4" ]; 
+then 
+    MODEL_NAME=$4 
+else 
+    MODEL_NAME="ensemble_model"
+fi
+
+if [ ! -z "$5" ]; 
+then 
+    CONTAINER_TAG=$5
+else 
+    CONTAINER_TAG="latest"
+fi
+
+if [ ! -z "$6" ]; 
+then 
+    MAX_BATCH_SIZE=$6
+else 
+    MAX_BATCH_SIZE="4" 
+fi
+
 ONNX_MODEL="temp_model"
 
-echo $MODEL_PATH
+NUM_CLASSES=$(python utils/count_classes.py $ID2LABEL_PATH)
+
 echo "Converting model to onnx"
 python utils/convert_to_onnx.py -m $MODEL_PATH -t $TOKENIZER_PATH -s $ONNX_MODEL
 
@@ -23,11 +45,17 @@ mv "$ONNX_MODEL/model.onnx" "model_repository/model_onnx/1"
 mv "$ONNX_MODEL"/*.json "model_repository/text_preprocessing/1/tokenizer"
 mv "$ONNX_MODEL"/vocab.txt "model_repository/text_preprocessing/1/tokenizer"
 # moving id2label
-cp $ID2LABEL_PATH "model_repository/post_processing/1"
-mv model_repository/ensemble_model model_repository/"$MODEL_NAME"
+cp $ID2LABEL_PATH "model_repository/post_processing/1/id2label.json"
+if [ ! $MODEL_NAME -eq "ensemble_model"];
+then
+    mv model_repository/ensemble_model model_repository/"$MODEL_NAME"
+fi
 
 echo "Building docker images"
-docker build -t triton_test:v2 . --no-cache
+docker build -t tritoned_$MODEL_NAME:$CONTAINER_TAG . --no-cache
 
 # cleanup
-bash cleanup.sh
+echo "Cleaning up"
+bash cleanup.sh "$MODEL_NAME"
+
+echo "Docker image tritoned_$MODEL_NAME:$CONTAINER_TAG successfully created"
